695a8626e74e8ba0d20a1b294ecec626db39ca6f
Paul Newton <paul@topolino.org>
2019-04-07 13:05:35 +0200

    Removed pickle files

--- doc-scanner/sentiment-nltk.py
+++ doc-scanner/sentiment-nltk.py
@@ -6,8 +6,8 @@ import nltk
 
 # SciKit classifiers
 from nltk.classify.scikitlearn import SklearnClassifier
-from sklearn.naive_bayes import MultinomialNB,BernoulliNB
-from sklearn.linear_model import LogisticRegression,SGDClassifier
+from sklearn.naive_bayes import MultinomialNB, BernoulliNB
+from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.svm import SVC, LinearSVC, NuSVC
 
 # init logging
@@ -17,27 +17,33 @@ for h in logger.handlers:
 handler = logging.StreamHandler()
 handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
 logger.addHandler(handler)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
+
 
 def saveit(classifier, filename):
     """Save the classifier to a pickle."""
-    
+
     logger.debug("Saving classifier to %s..." % filename)
-    classifier_f = open(filename,"wb")
+    classifier_f = open(filename, "wb")
     pickle.dump(classifier, classifier_f)
     classifier_f.close()
 
-    
+
 # our custom classifier
 from nltk.classify import ClassifierI
 from statistics import mode
+
+
 class VoteClassifier(ClassifierI):
     """Classifier based on a collection of other classifiers. Classifiers input based on the majority decisions of the sub-classifiers"""
 
     def __init__(self, classifiers):
-        self._classifiers = classifiers
-    
+        self._classifiers = classifiers.copy()  # copy the list in case it is changed elsewhere
+
     def classify(self, features):
+        """Classify the features using the list of internal classifiers. Classification is calculated by majority vote."""
+
+        #logger.debug("Classifying with %d classifiers" % len(self._classifiers))
         votes = []
         for c in self._classifiers:
             v = c.classify(features)
@@ -45,6 +51,7 @@ class VoteClassifier(ClassifierI):
         return mode(votes)
 
     def confidence(self, features):
+        """Return the confidence of the classification (rate of +ve votes / classifiers)."""
         votes = []
         for c in self._classifiers:
             v = c.classify(features)
@@ -53,9 +60,10 @@ class VoteClassifier(ClassifierI):
         choice_votes = votes.count(mode(votes))
         conf = choice_votes / len(votes)
         return conf
-    
+
 
 def find_features(word_features, document):
+    """Generate a set of features from a document."""
     words = set(document)
     features = {}
     for w in word_features:
@@ -64,56 +72,37 @@ def find_features(word_features, document):
     return features
 
 
-def create_classifiers():
+def create_classifier(c, desc, training_data, testing_data):
+    """Create a classifier of a specific type, train the classifier, evaluate the accuracy then save it to a .pickle file."""
+    logger.debug("Training %s" % desc)
+    trained_c = c.train(training_data)
+    logger.debug("%s accuracy percent: %f" % (desc, nltk.classify.accuracy(trained_c, testing_data) * 100))
+    saveit(trained_c, desc + ".pickle")
+
+    return trained_c
+
+
+def create_classifiers(training_data, testing_data):
+    """Create a set of classifiers."""
     logger.debug("Training classifiers...")
-    
-    classifier = nltk.NaiveBayesClassifier.train(training_set)
-    saveit(classifier, "naivebayes.pickle")
-    logger.debug("Naive Bayes accuracy percent: %f" % (nltk.classify.accuracy(classifier, testing_set)*100))
-    #classifier.show_most_informative_features(15)
-    
-    MNB_classifier = SklearnClassifier(MultinomialNB())
-    MNB_classifier.train(training_set)
-    saveit(MNB_classifier, "multinomialnb.pickle")
-    logger.debug("MNB_classifier accuracy percent: %f" % (nltk.classify.accuracy(MNB_classifier, testing_set)*100))
-    
-    BernoulliNB_classifier = SklearnClassifier(BernoulliNB())
-    BernoulliNB_classifier.train(training_set)
-    saveit(BernoulliNB_classifier, "bernouillinb.pickle")
-    logger.debug("BernoulliNB_classifier accuracy percent: %f", (nltk.classify.accuracy(BernoulliNB_classifier, testing_set)*100))
-    
-    LogisticRegression_classifier = SklearnClassifier(LogisticRegression())
-    LogisticRegression_classifier.train(training_set)
-    saveit(LogisticRegression_classifier, "logisticregression.pickle")
-    logger.debug("LogisticRegression_classifier accuracy percent: %f" % (nltk.classify.accuracy(LogisticRegression_classifier, testing_set)*100))
-    
-    SGDClassifier_classifier = SklearnClassifier(SGDClassifier())
-    SGDClassifier_classifier.train(training_set)
-    saveit(SGDClassifier_classifier, "sgd.pickle")
-    logger.debug("SGDClassifier_classifier accuracy percent: %f" % (nltk.classify.accuracy(SGDClassifier_classifier, testing_set)*100))
-    
-    ##SVC_classifier = SklearnClassifier(SVC())
-    ##SVC_classifier.train(training_set)
-    ##print("SVC_classifier accuracy percent:", (nltk.classify.accuracy(SVC_classifier, testing_set))*100)
-    
-    LinearSVC_classifier = SklearnClassifier(LinearSVC())
-    LinearSVC_classifier.train(training_set)
-    saveit(LinearSVC_classifier, "linearsvc.pickle")
-    logger.debug("LinearSVC_classifier accuracy percent: %f" % (nltk.classify.accuracy(LinearSVC_classifier, testing_set)*100))
-    
-    NuSVC_classifier = SklearnClassifier(NuSVC())
-    NuSVC_classifier.train(training_set)
-    saveit(NuSVC_classifier, "nusvc.pickle")
-    logger.debug("NuSVC_classifier accuracy percent: %f" % (nltk.classify.accuracy(NuSVC_classifier, testing_set)*100))
-    
-    # aggregate these into our custom classifier
-    classifiers = (classifier,
-                   NuSVC_classifier,
-                   LinearSVC_classifier,
-                   SGDClassifier_classifier,
-                   MNB_classifier,
-                   BernoulliNB_classifier,
-                   LogisticRegression_classifier)
+
+    classifiers = []
+
+    classifiers.append(create_classifier(nltk.NaiveBayesClassifier, "naivebayes", training_set, testing_data), "Naive Bayes classifier from NLTK")
+    classifiers.append(
+        create_classifier(SklearnClassifier(MultinomialNB()), "multinomialnb", training_data, testing_set), "Multinomial NB classifier from SciKit")
+    classifiers.append(create_classifier(SklearnClassifier(BernoulliNB()), "bernouillinb", training_data, testing_set), "Bernouilli NB classifier from SciKit")
+    classifiers.append(
+        create_classifier(SklearnClassifier(LogisticRegression()), "logisticregression", training_data, testing_set), "Logistic Regression classifier from SciKit")
+    classifiers.append(create_classifier(SklearnClassifier(SGDClassifier()), "sgd", training_data, testing_data), "SGD classifier from SciKit")
+    classifiers.append(
+        create_classifier(SklearnClassifier(LinearSVC()), "linearsvc.pickle", training_data, testing_data), "Linear SVC classifier from SciKit")
+    classifiers.append(create_classifier(SklearnClassifier(NuSVC()), "nusvc.pickle", training_data, testing_data), "Nu SVC classifier from SciKit")
+
+    voted_classifier = VoteClassifier(classifiers)
+    saveit(voted_classifier, "voting.pickle")
+
+    classifiers.insert(0, VoteClassifier(classifiers), "Voting Classifier")
 
     return classifiers
 
@@ -125,47 +114,45 @@ def create_feature_sets():
         nltk.set_proxy(os.environ["NLTK_PROXY"])
     nltk.download('movie_reviews')
     from nltk.corpus import movie_reviews
-    
+
     logger.debug("Building data set...")
-    
+
     # build list of words (1 list per doc) and pos/neg category
     documents = [(list(movie_reviews.words(fileid)), category)
                  for category in movie_reviews.categories()
                  for fileid in movie_reviews.fileids(category)]
     random.shuffle(documents)
-    
+
     # extract list of features from most common words
     all_words = []
     for w in movie_reviews.words():
         all_words.append(w.lower())
-    
+
     all_words = nltk.FreqDist(all_words)
     logger.debug("Frequency dist of 15 most common words:%s" % all_words.most_common(15))
     logger.debug("Frequency of 'stupid':%d" % all_words["stupid"])
     word_features = list(all_words.keys())[:3000]
-    
-    # build feature sets from data
+
+    # build list of feature sets from data
     return [(find_features(word_features, rev), category) for (rev, category) in documents]
-    
 
-feature_sets = create_feature_sets()        
-training_set = feature_sets[:1900]
-testing_set =  feature_sets[1900:]
 
-if False:
-    classifiers = create_classifiers()
-    voted_classifier = VoteClassifier(classifiers)
-    saveit(voted_classifier, "voted.pickle")
+if __name__ == "__main__":
+    feature_sets = create_feature_sets()
+    training_set = feature_sets[:1900]
+    testing_set = feature_sets[1900:]
 
-classifier_f = open("voted.pickle","rb")
-voted_classifier = pickle.load(classifier_f)
-classifier_f.close()
-    
-logger.info("Classification: %s Confidence: %f" % (voted_classifier.classify(testing_set[0][0]), voted_classifier.confidence(testing_set[0][0])*100))
-logger.info("Classification: %s Confidence: %f" % (voted_classifier.classify(testing_set[1][0]), voted_classifier.confidence(testing_set[0][0])*100))
-logger.info("Classification: %s Confidence: %f" % (voted_classifier.classify(testing_set[2][0]), voted_classifier.confidence(testing_set[0][0])*100))
-logger.info("Classification: %s Confidence: %f" % (voted_classifier.classify(testing_set[3][0]), voted_classifier.confidence(testing_set[0][0])*100))
-logger.info("Classification: %s Confidence: %f" % (voted_classifier.classify(testing_set[4][0]), voted_classifier.confidence(testing_set[0][0])*100))
-logger.info("Classification: %s Confidence: %f" % (voted_classifier.classify(testing_set[5][0]), voted_classifier.confidence(testing_set[0][0])*100))
-logger.info("Classification: %s Confidence: %f" % (voted_classifier.classify(testing_set[0][0]), voted_classifier.confidence(testing_set[0][0])*100))
-logger.info("voted_classifier accuracy percent: %f" % (nltk.classify.accuracy(voted_classifier, testing_set)*100))
+    train = True
+
+    if train:
+        classifiers = create_classifiers(training_set, testing_set)
+        voted_classifier = classifiers[0]
+    else:
+        classifier_f = open("voted.pickle", "rb")
+        voted_classifier = pickle.load(classifier_f)
+        classifier_f.close()
+
+    for data in testing_set[:20]:
+        logger.info("Classification: %s Confidence: %f" % (
+        voted_classifier.classify(data[0]), voted_classifier.confidence(data[0]) * 100))
+    logger.info("voted_classifier accuracy percent: %f" % (nltk.classify.accuracy(voted_classifier, testing_set) * 100))
