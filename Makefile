c42be92b5ba9d73dcbb0be06c8884b368d96c2c2
Paul Newton <paul@topolino.org>
2019-11-01 23:33:02 +0100

    Added pre-commit hooks

--- Makefile
+++ Makefile
@@ -51,7 +51,6 @@ clean-test: ## remove test and coverage artifacts
 	rm -fr .pytest_cache
 
 lint: ## check style with flake8
-	black twitter_ml
 	flake8 twitter_ml tests
 
 test: ## run tests quickly with the default Python
--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+"""Sphinx doc configuration."""
 #
 # twitter_ml documentation build configuration file, created by
 # sphinx-quickstart on Fri Jun  9 13:47:02 2017.
@@ -21,7 +21,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath(".."))
 
 import twitter_ml
 
@@ -33,22 +33,22 @@ import twitter_ml
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'm2r']
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "m2r"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ".rst"
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # General information about the project.
-project = u'Twitter ML'
+project = u"Twitter ML"
 copyright = u"2019, Paul Newton"
 author = u"Paul Newton"
 
@@ -71,10 +71,10 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
@@ -84,7 +84,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
@@ -95,12 +95,12 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # -- Options for HTMLHelp output ---------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'twitter_mldoc'
+htmlhelp_basename = "twitter_mldoc"
 
 # -- Options for LaTeX output ------------------------------------------
 
@@ -108,15 +108,12 @@ latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
-
     # The font size ('10pt', '11pt' or '12pt').
     #
     # 'pointsize': '10pt',
-
     # Additional stuff for the LaTeX preamble.
     #
     # 'preamble': '',
-
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
@@ -126,20 +123,20 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass
 # [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'twitter_ml.tex',
-     u'Twitter ML Documentation',
-     u'Paul Newton', 'manual'),
+    (
+        master_doc,
+        "twitter_ml.tex",
+        u"Twitter ML Documentation",
+        u"Paul Newton",
+        "manual",
+    )
 ]
 
 # -- Options for manual page output ------------------------------------
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'twitter_ml',
-     u'Twitter ML Documentation',
-     [author], 1)
-]
+man_pages = [(master_doc, "twitter_ml", u"Twitter ML Documentation", [author], 1)]
 
 # -- Options for Texinfo output ----------------------------------------
 
@@ -147,10 +144,13 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'twitter_ml',
-     u'Twitter ML Documentation',
-     author,
-     'twitter_ml',
-     'One line description of project.',
-     'Miscellaneous'),
+    (
+        master_doc,
+        "twitter_ml",
+        u"Twitter ML Documentation",
+        author,
+        "twitter_ml",
+        "One line description of project.",
+        "Miscellaneous",
+    )
 ]
