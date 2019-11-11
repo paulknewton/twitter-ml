46b9433ed5b594691da7baa11c97261b7114a7a6
Paul Newton <paul@topolino.org>
2019-11-11 13:02:23 +0100

    Fixed sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -21,10 +21,8 @@
 import os
 import sys
 
+sys.path.insert(0, os.path.abspath(".."))
 import twitter_ml
-
-sys.path.insert(0, os.path.abspath("../twitter_ml"))
-
 
 # -- General configuration ---------------------------------------------
 
